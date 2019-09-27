4551983f45a9cfe4af77cd41a422bdc81d701d8b
rozcovo <israel.batista@univem.edu.br>
2019-09-26 22:38:32 -0300

    Segundo e último dia do curso

--- client/js/chat.js
+++ client/js/chat.js
@@ -11,7 +11,68 @@ function retornaUsuario(id) {
 
 let socket = io('ws://localhost:3000');
 
+let $form = document.querySelector('#form-chat');
+let $input = document.querySelector('#input-chat');
+let $conversationMensages = document.querySelector('#mensagens-chat');
+
+$form.addEventListener('submit',
+function(event) {
+
+    event.preventDefault();
+
+    let mensagem = $input.value;
+    $input.value = '';
+
+    if (mensagem === '') {
+
+        return;
+    }
+
+    socket.emit('criarMensagem', {
+       usuario: retornaUsuario(socket.id),
+       mensagem
+    });
+});
+
 socket.on('connect', function() {
 
     socket.emit('criarUsuario', retornaUsuario(socket.id));
+});
+
+socket.on('listarMensagens', function(mensagens) {
+
+    $conversationMensages.innerHTML = '';
+
+    mensagens
+    .forEach(({ mensagem, usuario }) => {
+
+        let $text = document.createElement('p');
+        $text.classList.add('message__text');
+        $text.textContent = mensagem;
+
+        let $box = document.createElement('div');
+        $box.classList.add('message__box');
+
+        let $mensagem = document.createElement('div');
+        $mensagem.classList.add('message');
+
+        if (usuario.usuario === retornaUsuario(socket.id).usuario) {
+
+            $mensagem.classList.add('message--sent');
+        }
+        else {
+
+            $mensagem.classList.add('message--received');
+
+            let $user = document.createElement('p');
+            $user.classList.add('message__user');
+            $user.textContent = usuario.usuario;
+            $box.append($user);
+        }
+
+        $box.append($text);
+        $mensagem.append($box);
+        $conversationMensages.append($mensagem);
+        $conversationMensages.scrollTop = $conversationMensages.scrollHeight;
+    });
 });
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const socketIO = require('socket.io');
 let io = socketIO(3000);
 
 let usuarios = [];
+let mensagens = [];
 
 io.on('connection', function(socket) {
 
@@ -11,6 +12,16 @@ io.on('connection', function(socket) {
 
         usuarios.push(dadosUsuario);
         console.log('[server] NOVO USUÁRIO CRIADO => ', dadosUsuario);
+        console.log('[server] LISTA DE USUÁRIOS => ', usuarios);
+    });
+
+    socket.on('criarMensagem', function(mensagem) {
+
+        mensagens.push(mensagem);
+        console.log('[server] NOVA MENSAGEM => ', mensagem);
+        console.log('[server] LISTA DE MENSAGENS => ', mensagens);
+
+        io.emit('listarMensagens', mensagens);
     });
 
     socket.on('disconnect', function() {
