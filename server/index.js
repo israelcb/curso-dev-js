adf00838c4fb90ac78b93815e2d4b22d2d837f84
rozcovo <israel.batista@univem.edu.br>
2019-09-26 22:38:32 -0300

    Segundo e último dia do curso

--- server/index.js
+++ server/index.js
@@ -2,15 +2,27 @@ const socketIO = require('socket.io');
 let io = socketIO(3000);
 
 let usuarios = [];
+let mensagens = [];
 
 io.on('connection', function(socket) {
 
     console.log('[server] NOVO USUÁRIO CONECTADO');
+    io.emit('listarMensagens', mensagens);
 
     socket.on('criarUsuario', function(dadosUsuario) {
 
         usuarios.push(dadosUsuario);
         console.log('[server] NOVO USUÁRIO CRIADO => ', dadosUsuario);
+        console.log('[server] LISTA DE USUÁRIOS => ', usuarios);
+    });
+
+    socket.on('criarMensagem', function(mensagem) {
+
+        mensagens.push(mensagem);
+        console.log('[server] NOVA MENSAGEM => ', mensagem);
+        console.log('[server] LISTA DE MENSAGENS => ', mensagens);
+
+        io.emit('listarMensagens', mensagens);
     });
 
     socket.on('disconnect', function() {
